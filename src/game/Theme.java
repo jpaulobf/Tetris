ba7027a4ce89b1db3d240951ff477e280268917e
jpaulobf <jpaulobf@gmail.com>
2022-03-29 21:56:32 -0300

    New elements to the theme

--- src/game/Theme.java
+++ src/game/Theme.java
@@ -12,12 +12,14 @@ public class Theme {
     private Color lineColor             = null;
     private BufferedImage next         	= null;
 	private BufferedImage hold         	= null;
+    private BufferedImage circle       	= null;
     private String [] bgThemes          = {"background1", "background2", "background3", "background4", "background5", "background6", "background7", "background8"};
-    private String [] nextLabels        = {"next_bl", "next_w", "next_w", "next_bb", "next_b", "next_bb", "next_bb", "next_w"};
-    private String [] holdLabes         = {"hold_bl", "hold_w", "hold_w", "hold_bb", "hold_b", "hold_bb", "hold_bb", "hold_w"};
+    private String [] nextLabels        = {"next_ww", "next_w", "next_w", "next_bb", "next_b", "next_bb", "next_bb", "next_w"};
+    private String [] holdLabes         = {"hold_ww", "hold_w", "hold_w", "hold_bb", "hold_b", "hold_bb", "hold_bb", "hold_w"};
     private boolean [] filledThemes     = {true, false, false, false, false, false, false, false};
     private Color [] boardColors        = {new Color(1f, 1f, 1f, 1f),       new Color(0f, 0f, 0f, 0.4f),    new Color(0f, 0f, 0f, 0.8f),    new Color(0f, 0f, 0f, 0.3f),        new Color(0.1f, 0.1f, 0.1f, 0.3f),      new Color(1, 1, 1, 0.6f),       new Color(1f, 1f, 1f, 0.6f),      new Color(0.3f, 0.5f, 0.9f, 0.4f)};
     private Color [] lineColors         = {new Color(1f, 0.8f, 0.2f, 0.2f), new Color(0f, 0f, 0f, 0.1f),    new Color(1f, 1f, 1f, 0.2f),    new Color(1, 0.8f, 0.2f, 0.4f),     new Color(0f, 0f, 0f, 0.2f),            new Color(0f, 0f, 0f, 0.05f),   new Color(0f, 0f, 0f, 0.05f),     new Color(1f, 1f, 1f, 0.2f)};
+    private String[] circles            = {"circle100", "circle", "circle", "circle100", "circle", "circle", "circle100", "circle"};
 
     /**
      * Constructor
@@ -25,6 +27,10 @@ public class Theme {
      */
     public Theme(byte theme) {
         this.setTheme(theme);
+    }
+
+    public BufferedImage getCircle() {
+        return (this.circle);
     }
 
     /**
@@ -38,6 +44,7 @@ public class Theme {
         this.lineColor      = this.lineColors[theme];
         this.next           = (BufferedImage)LoadingStuffs.getInstance().getStuff(nextLabels[theme]);
         this.hold           = (BufferedImage)LoadingStuffs.getInstance().getStuff(holdLabes[theme]);
+        this.circle         = (BufferedImage)LoadingStuffs.getInstance().getStuff(circles[theme]);
     }
 
     //getters
