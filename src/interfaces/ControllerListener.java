016478c77752f160ba8db62bbaa465fb1ca66ca1
jpaulobf <jpaulobf@gmail.com>
2022-04-05 18:46:32 -0300

    New Listener

--- src/interfaces/ControllerListener.java
+++ src/interfaces/ControllerListener.java
@@ -6,5 +6,8 @@ public interface ControllerListener {
     public void notify(boolean U, 
                        boolean D, 
                        boolean L, 
-                       boolean R);
+                       boolean R, 
+                       boolean HOLD, 
+                       boolean DROP, 
+                       boolean ROTATE);
 }
