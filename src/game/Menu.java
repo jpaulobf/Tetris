83a5f2f67b0b4a6e14448818fc9699d71e06b579
jpaulobf <jpaulobf@gmail.com>
2022-04-14 22:16:59 -0300

    Initial Menu class

---
+++ src/game/Menu.java
@@ -0,0 +1,19 @@
+package game;
+
+public class Menu {
+    
+    private volatile long framecounter = 0L;
+
+    public Menu() {
+
+    }
+
+    public synchronized void update(long frametime) {
+
+    }
+    
+    public synchronized void draw(long frametime) {
+
+    }
+
+}
