76cd1b8255ac1cc1b260d2f1b8c08fbc112efcd5
jpaulobf <jpaulobf@gmail.com>
2022-06-05 23:02:01 -0300

    Exit screen (WIP)

--- src/game/ExitScreen.java
+++ src/game/ExitScreen.java
@@ -8,18 +8,25 @@ import java.awt.geom.Rectangle2D;
  */
 public class ExitScreen {
 
-    private int positionX                       = 0;
-    private int positionY                       = 0;
-    private int width                           = 300;
-    private int height                          = 150;
-    private int buttonWidth                     = 50;
-    private int buttonHeight                    = 20;
+    private short positionX                     = 0;
+    private short positionY                     = 0;
+    private short width                         = 300;
+    private short height                        = 150;
+    private short buttonWidth                   = 50;
+    private short buttonHeight                  = 20;
     private int resolutionH                     = 0;
     private int resolutionW                     = 0;
     private Graphics2D g2d                      = null;
     private Rectangle2D.Double mainBox          = null;
     private Rectangle2D.Double yesBox           = null;
     private Rectangle2D.Double noBox            = null;
+    private short mainBoxMinWidth               = 0;
+    private short mainBoxMinHeight              = 0;
+    private short mainBoxCurWidth               = this.mainBoxMinWidth;
+    private short mainBoxCurHeight              = this.mainBoxMinHeight;
+    private volatile long framecounter          = 0;
+    private final byte step                     = 10;
+    private final byte halfStep                 = step/2;
 
     /**
      * Constructor
@@ -32,8 +39,8 @@ public class ExitScreen {
         this.resolutionW = resolutionW;
         this.resolutionH = resolutionH;
 
-        this.positionX = (int)((resolutionW / 2) - (this.width / 2));
-        this.positionY = (int)((resolutionH / 2) - (this.height / 2));
+        this.positionX = (short)((resolutionW / 2) - (this.width / 2));
+        this.positionY = (short)((resolutionH / 2) - (this.height / 2));
 
         this.mainBox = new Rectangle2D.Double(this.positionX, this.positionY, width, height);
         this.yesBox = new Rectangle2D.Double(this.positionX, this.positionY, buttonWidth, buttonHeight);
@@ -44,11 +51,34 @@ public class ExitScreen {
     }
 
     public void draw(long frametime) {
-       //TODO
+       
+
+
+
     }
 
+    /**
+     * Update the exit screen window
+     * @param frametime
+     */
     public void update(long frametime) {
-        //TODO
+        
+        this.framecounter += frametime;
+        
+        if (this.mainBoxCurWidth < this.width) {
+            if (this.framecounter > 100_000_000) {
+                
+                this.mainBoxCurWidth += step;
+                this.mainBoxCurHeight += halfStep;
+
+                this.framecounter = 0;
+            }
+        } else {
+            if (this.mainBoxCurWidth > this.width || this.mainBoxCurHeight > this.height) {
+                this.mainBoxCurWidth = this.width;
+                this.mainBoxCurHeight = this.height;
+            }
+        }
     }
 
     public void move(int keyCode) {
