66dee643ffcc743c7fac05023281d7640d6615eb
jpaulobf <jpaulobf@gmail.com>
2022-06-28 22:23:37 -0300

    fullscreen enabled

--- src/engine/Tetris.java
+++ src/engine/Tetris.java
@@ -62,7 +62,7 @@ public class Tetris implements Runnable {
         private boolean showFPS                     = true;
 
         //control and fullscreen controller
-        private boolean fullscreen                  = false;
+        private boolean fullscreen                  = true;
         private boolean isFullScreenAvailable       = false;
 
         /**
